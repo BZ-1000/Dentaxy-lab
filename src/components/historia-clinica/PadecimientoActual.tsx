f2c859110c13206a81d99717cd48f371942366e0
DentalBasicsAcademy1 <zavalabrau2018@gmail.com>
2025-02-08 21:22:54 -0600

    c

--- src/components/historia-clinica/PadecimientoActual.tsx
+++ src/components/historia-clinica/PadecimientoActual.tsx
@@ -149,7 +149,7 @@ const PadecimientoActual = ({
 
         {/* Botón Generar Redacción IA */}
         {!showRedaccion && (
-          <div className="p-6 flex justify-end">
+          <div className="p-6 flex justify-center">
             <Button onClick={generarRedaccionIA} className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
               Generar Redacción IA
             </Button>
