2c73d1b2111a0ccc9950ca18e18e74590cefd031
DentalBasicsAcademy1 <zavalabrau2018@gmail.com>
2025-02-23 19:59:29 -0600

    c

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -79,19 +79,20 @@ const Landing = () => {
             Dental Basics Academy
           </span>
         </div>
-        <div className="flex-grow flex justify-center">
-          <MenuBar
-            items={menuItems}
-            activeItem={activeItem}
-            onItemClick={setActiveItem}
-            className="py-1 text-shadow"
-          />
-        </div>
-        <div className="w-32"></div> {/* Espacio vacío para centrar el menú */}
+      </div>
+
+      {/* Menú centrado */}
+      <div className="relative z-10 flex justify-center mt-2">
+        <MenuBar
+          items={menuItems}
+          activeItem={activeItem}
+          onItemClick={setActiveItem}
+          className="py-1 text-shadow"
+        />
       </div>
 
       {/* Contenido principal */}
-      <div className="relative z-10 flex min-h-[calc(100vh-80px)] flex-col items-center justify-center px-4">
+      <div className="relative z-10 flex min-h-[calc(100vh-120px)] flex-col items-center justify-center px-4">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
