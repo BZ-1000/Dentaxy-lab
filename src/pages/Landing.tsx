068edfdc6d203bda4aea41661dcfb1a809ab1797
DentalBasicsAcademy1 <zavalabrau2018@gmail.com>
2025-02-24 01:48:46 -0600

    c

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -91,8 +91,8 @@ const Landing = () => {
                 key={index}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
-                transition={{ delay: 0.5 + index * 0.05, duration: 1 }}
-                exit={{ opacity: 0, transition: { delay: 3 } }}
+                transition={{ delay: 0.05 + index * 0.05, duration: 0.05 }}
+                exit={{ opacity: 0, transition: { delay: 0 } }}
               >
                 {letter}
               </motion.span>
