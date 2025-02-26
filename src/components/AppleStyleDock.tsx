0dd26e8d6838fa158d9095fcf1c31b70e5bd6305
DentalBasicsAcademy1 <zavalabrau2018@gmail.com>
2025-02-25 19:30:58 -0600

    c

--- src/components/AppleStyleDock.tsx
+++ src/components/AppleStyleDock.tsx
@@ -44,9 +44,9 @@ const data = [
     href: '#',
   },
   {
-    title: 'Feedback',
+    title: 'Mejoras',
     icon: (
-      <Mail className='h-full w-full text-neutral-600 dark:text-neutral-300' />
+      <Mail className='h-full w-full text-red-500 dark:text-red-400' /> // Color rojo estilo Apple
     ),
     href: '#',
   },
