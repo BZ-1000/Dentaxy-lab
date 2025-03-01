e7ba1079552ae154890898153730c815f3b77017
DentalBasicsAcademy1 <zavalabrau2018@gmail.com>
2025-03-01 15:07:45 -0600

    c

--- src/components/historia-clinica/PadecimientoActual.tsx
+++ src/components/historia-clinica/PadecimientoActual.tsx
@@ -326,7 +326,7 @@ El paciente refiere la presencia de dolor localizado en ${localizacion.descripci
                   className="min-h-[100px] max-h-[200px] w-full resize-y bg-gray-50 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-md"
                 />
                 {formData.padecimientoActual.motivoConsulta === defaultMotivoConsulta && (
-                  <div className="absolute top-2 left-[185px] pointer-events-none flex items-center">
+                  <div className="absolute top-2 left-[200px] pointer-events-none flex items-center">
                     <Typewriter
                       text={motivosEjemplo}
                       speed={50}
