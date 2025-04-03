a4bff3f0e91e98606514f9ba356d073ebea13e77
DentalBasicsAcademy1 <zavalabrau2018@gmail.com>
2025-04-02 21:00:33 -0600

    c

--- src/components/historia-clinica/CaracteristicasDolor.tsx
+++ src/components/historia-clinica/CaracteristicasDolor.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useRef, useEffect } from "react";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
