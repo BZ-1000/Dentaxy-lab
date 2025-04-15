0b0abf3f25380e8a6126b4abc7bafe4d430daa45
DentalBasicsAcademy1 <zavalabrau2018@gmail.com>
2025-04-14 20:19:38 -0600

    mujer rosa

--- src/components/HistoriaClinica.tsx
+++ src/components/HistoriaClinica.tsx
@@ -462,7 +462,7 @@ const HistoriaClinica = () => {
                 <button
                   className={`px-4 py-2 rounded-md text-sm transition-colors ${
                     !esMujer
-                      ? 'bg-[#2ecc71] text-white'
+                      ? 'bg-[fuchsia-500] text-white'
                       : 'bg-gray-100 dark:bg-gray-700'
                   }`}
                   onClick={() => setEsMujer(false)}
